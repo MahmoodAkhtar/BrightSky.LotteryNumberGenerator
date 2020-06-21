9c70d3284ae87bf6288a17c3f7f939d9af224d8e
Mahmood Akhtar <mahmood233@hotmail.com>
2020-06-21 20:11:33 +0100

    Finishing off unit tests to cover RgbColour class.

--- test/BrightSky.LotteryNumberGenerator.Core.Tests/Types/RgbColourTests.cs
+++ test/BrightSky.LotteryNumberGenerator.Core.Tests/Types/RgbColourTests.cs
@@ -1,4 +1,5 @@
 ﻿using FluentAssertions;
+using System;
 using Xunit;
 
 namespace BrightSky.LotteryNumberGenerator.Core.Tests.Types
@@ -18,6 +19,63 @@ namespace BrightSky.LotteryNumberGenerator.Core.Tests.Types
             actual.Red.Should().Be(red);
             actual.Green.Should().Be(green);
             actual.Blue.Should().Be(blue);
+        }
+
+        [Theory, AutoDomainData]
+        public void Constructor_Given_Null_Red_Arg_Throws_ArgumentNullExpection(RgbValue red, RgbValue green, RgbValue blue)
+        {
+            // Arrange
+            red = null;
+
+            Action act = () => new RgbColour(red, green, blue);
+
+            // Act & Assert
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [Theory, AutoDomainData]
+        public void Constructor_Given_Null_Green_Arg_Throws_ArgumentNullExpection(RgbValue red, RgbValue green, RgbValue blue)
+        {
+            // Arrange
+            green = null;
+
+            Action act = () => new RgbColour(red, green, blue);
+
+            // Act & Assert
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [Theory, AutoDomainData]
+        public void Constructor_Given_Null_Blue_Arg_Throws_ArgumentNullExpection(RgbValue red, RgbValue green, RgbValue blue)
+        {
+            // Arrange
+            blue = null;
+
+            Action act = () => new RgbColour(red, green, blue);
+
+            // Act & Assert
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [Theory, AutoDomainData]
+        public void Two_Objects_Given_Same_Constructor_Args_Should_Be_Equal(RgbValue red, RgbValue green, RgbValue blue)
+        {
+            // Arrange & Act
+            var a = new RgbColour(red, green, blue);
+            var b = new RgbColour(red, green, blue);
+
+            // Assert
+            a.Should().Be(b);
+        }
+
+        [Theory, AutoDomainData]
+        public void ToString_Should_Be_Correct_Format(RgbColour colour)
+        {
+            // Arrange
+            var expected = $"rgb({colour.Red}, {colour.Green}, {colour.Blue})";
+
+            // Act & Assert
+            colour.ToString().Should().Be(expected);
         }
     }
 }
